1e47e22ec87e27b34865b8784c7120c553168484
Hamooz9667 <hamzaahmed8911@gmail.com>
2026-02-17 19:29:06 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.WriteLine("enter the first number");
+            while (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            Console.WriteLine("enter the second Number");
+            while (!double.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("invalid input. Please enter a valid number.");
+            }
+            if (num1 > num2)
+            {
+                Console.WriteLine("the first number which was " + num1 + " is the higher number");
+            }
+            if (num1 < num2) { Console.WriteLine("The second number which was " + num2 + " is the higher number"); }
+            if (num1 == num2) { Console.WriteLine("both number are equal"); }
+        }
+    }
+}
