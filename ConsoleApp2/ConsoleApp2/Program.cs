010fe52fb42fff8b06fa40512b391113982989ed
Hamooz9667 <hamzaahmed8911@gmail.com>
2026-02-17 19:20:40 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("enter a number");
+            while (!double.TryParse(Console.ReadLine(), out num))
+                Console.WriteLine("please enter a valid number");
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("the number is even");
+            }
+            else { Console.WriteLine("number is odd"); }
+        }
+    }
+}
