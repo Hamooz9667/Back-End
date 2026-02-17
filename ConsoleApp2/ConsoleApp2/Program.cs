fb3079ae78df973253d87282d02683ad22d8d5ae
Hamooz9667 <hamzaahmed8911@gmail.com>
2026-02-17 19:31:44 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 101);
+            int attemptsLeft = 5;
+            bool hasWon = false;
+
+            Console.WriteLine("I have chosen a number between 1 and 100.");
+            Console.WriteLine("You have " + attemptsLeft + " attempts to guess it.");
+
+            while (attemptsLeft > 0 && !hasWon)
+            {
+                int currentAttempt = 6 - attemptsLeft;
+                Console.Write("\nAttempt " + currentAttempt + "/5 - Enter your guess: ");
+
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int guess))
+                {
+                    if (guess == secretNumber)
+                    {
+                        Console.WriteLine("Correct! You won!");
+                        hasWon = true;
+                    }
+                    else if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Too high! Try again.");
+                        attemptsLeft--;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too low! Try again.");
+                        attemptsLeft--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please enter a whole number.");
+                }
+            }
+
+            if (!hasWon)
+            {
+                Console.WriteLine("Game Over! You ran out of attempts. The number was " + secretNumber + ".");
+            }
+
+
+        }
+    }
+}
