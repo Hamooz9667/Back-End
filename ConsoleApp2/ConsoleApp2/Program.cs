a6c39be9078665bbc70311a1a7cb41ce8a5c3e4c
Hamooz9667 <hamzaahmed8911@gmail.com>
2026-02-17 19:33:55 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static double Factorial(double number)
+        {
+            if (number == 0 || number == 1)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            double number;
+            Console.WriteLine("enter a factorial number");
+            while (!double.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("enter a valid number");
+            }
+            Console.WriteLine("the factorial of " + number + " is " + Factorial(number));
+
+
+        }
+    }
+}
